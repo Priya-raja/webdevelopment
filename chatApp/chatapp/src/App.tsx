05f35a6dc95d7ee68f457aeacc02df8128abd3a1
Priya-raja <priya.thevar89@gmail.com>
2025-06-24 01:29:57 +0400

    added login validation

--- chatApp/chatapp/src/App.tsx
+++ chatApp/chatapp/src/App.tsx
@@ -1,13 +1,10 @@
-
-
-
+import Auth from './components/Auth';
 function App() {
-  
 
   return (
     <>
       <div>
-        <h1> Hello </h1>
+        <Auth/>
       </div>
     </>
   )
--- chatApp/chatapp/src/components/Auth.tsx
+++ chatApp/chatapp/src/components/Auth.tsx
@@ -1,18 +1,60 @@
-import React,{useState} from 'react'
 
-enum AuthType {
-    Login = 'Login',
-    SignUp ='SignUp'
-} 
+import { useState} from 'react'
+import {useForm} from 'react-hook-form'
+
+const AuthType = {
+    Login: "login",
+    SignUp: "signup"
+} as const
+
+
+
+interface AuthForm {
+    email: string;
+    password: string;
+}
+
 
 
 const Auth = () => {
-    
-    const [authType, setAuthType] = useState<AuthType>(AuthType.SignUp)
+    const [authType, setAuthType] = useState<"login" | "signup">(AuthType.Login);
 
+const {register, handleSubmit, formState: { errors }} = useForm<AuthForm>();
+    const onSubmit = (data: AuthForm) => {
+        console.log(data);
+    }
   return (
-    <div>Auth</div>
+    <div className="auth-container">
+    
+        <h2>{authType === AuthType.Login ? "Login" : "Sign Up"}</h2>
+        
+      <form onSubmit ={handleSubmit(onSubmit)}>
+        <label> Email:</label>
+        <input type="text" placeholder="Enter your email" 
+        {...register("email", {required: "email is required"})}
+        />
+        {errors.email && <p className='error-text'> {errors.email.message}</p>}
+
+        <label> Password</label>
+        <input type="password" placeholder="Enter your password"
+        {...register("password", {required: "password is required", minLength: 6})}
+        />
+        {errors.password && <p className='error-text'> {errors.password.message}</p>}
+        <button type='submit'>
+        {authType === AuthType.Login ? "Login" : "Sign Up"} 
+        </button>    
+            
+    </form>
+
+    <div>
+        {authType === AuthType.Login ? (
+            <p>Don't have an account? <button onClick={() => setAuthType(AuthType.SignUp)} className="auth-link">{AuthType.SignUp}</button></p>
+        ) :
+            <p>Already have an account? <button onClick={() => setAuthType(AuthType.Login)} className="auth-link">{AuthType.Login}</button></p>
+        }
+    </div>
+    </div>
   )
 }
 
-export default Auth;
+export default Auth
